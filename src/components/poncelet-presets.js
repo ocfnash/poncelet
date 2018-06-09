64ae7a5b100f1b7b5b092efc3c64ad75e7ab9bc4
Oliver Nash <oliver.nash@gmail.com>
2018-06-09 15:58:25 +0100

    Redesign preset component and reshuffle layout.
    Pushing the paramter values for the presets into tooltip titles
    is a big win. Further tweaking required but layout is getting close.

--- src/components/poncelet-presets.js
+++ src/components/poncelet-presets.js
@@ -12,51 +12,27 @@ class PonceletPresets extends Component {
     const j = (m*m - m + 1)**3 / (m*m-m)**2;
     const u = (alpha + beta - alpha*beta) / (alpha - beta + alpha*beta);
     const theta = arccn / (4*ellipticK);
-
+    const fullName = `${name} (${Math.round(n*theta)}/${n})`;
     const rnd = (x, d) => Number(x).toFixed(d);
+    const parameterValues =
+        `a=${rnd(a, 4)}, b=${rnd(b, 4)}, α=${rnd(alpha, 4)}, β=${rnd(beta, 4)}, ` +
+        `m=${rnd(m, 4)}, j=${rnd(j, 4)}, u=${rnd(u, 4)}, ` +
+        `K(m)=${rnd(ellipticK, 4)}, arccn(u, m)=${rnd(arccn, 4)}, θ=${rnd(theta, 6)}`;
     const onClick = () => this.props.selectParams(n, a, b, r);
 
     return (
-      <tr className="poncelet-preset-row" key={key} >
-        <td className="poncelet-preset-cell-name" onClick={onClick}>{name} ({Math.round(n*theta)}/{n})</td>
-        <td className="poncelet-preset-cell">{rnd(a, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(b, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(alpha, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(beta, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(m, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(j, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(u, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(ellipticK, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(arccn, 4)}</td>
-        <td className="poncelet-preset-cell">{rnd(theta, 6)}</td>
-      </tr>
+      <div className="poncelet-preset-selector" key={key} onClick={onClick} title={parameterValues} >
+        {fullName}
+      </div>
     );
   }
   render() {
     const presets = presetData.map((data, i) =>
       this.renderPreset(`param-preset-${i}`, data));
-
     return (
-      <table className="poncelet-presets-table">
-        <thead>
-          <tr className="poncelet-preset-header">
-            <td className="poncelet-preset-cell"></td>
-            <td className="poncelet-preset-cell">a</td>
-            <td className="poncelet-preset-cell">b</td>
-            <td className="poncelet-preset-cell">α</td>
-            <td className="poncelet-preset-cell">β</td>
-            <td className="poncelet-preset-cell">m</td>
-            <td className="poncelet-preset-cell">j</td>
-            <td className="poncelet-preset-cell">u</td>
-            <td className="poncelet-preset-cell">K(m)</td>
-            <td className="poncelet-preset-cell">arccn(u, m)</td>
-            <td className="poncelet-preset-cell">θ</td>
-          </tr>
-        </thead>
-        <tbody>
-          {presets}
-        </tbody>
-      </table>
+      <div className="poncelet-presets-container">
+        {presets}
+      </div>
     );
   }
 }
